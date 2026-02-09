0b2c19771049de1d60bcc8c92521f317377d6a74
roxtarankit <ankitsinghas12343@gmail.com>
2026-02-09 14:41:34 +0530

    version1

---
+++ script.js
@@ -0,0 +1,21 @@
+var img=document.querySelector('img')
+var love=document.querySelector('#love')
+
+
+img.addEventListener('dblclick',function(){
+    love.style.opacity=1
+    love.style.transform='translate(-50%,-50%) scale(1) rotate(0deg)'
+
+    setTimeout(function(){
+        love.style.transform='translate(-50%,-350%) scale(1) rotate(0deg)'
+    },800)
+
+    setTimeout(function(){
+        love.style.opacity=0
+    },1000)
+
+    setTimeout(function(){
+        love.style.opacity=0
+        love.style.transform='translate(-50%,-50%) scale(0) rotate(0deg)'
+    },1200)    
+})
